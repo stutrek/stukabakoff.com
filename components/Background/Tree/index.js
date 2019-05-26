9e042c3a5a1d914444ee604dd05989a00f475a17
Stu Kabakoff <sakabako@gmail.com>
2019-05-25 20:12:02 -0400

    fix blurry reload

--- components/Background/Tree/index.js
+++ components/Background/Tree/index.js
@@ -7,15 +7,19 @@ import styles from '../background.module.css';
 export default props => {
     const canvasRef = useRef(undefined);
     useLayoutEffect(() => {
-        const canvas = canvasRef.current;
-        canvas.width = canvas.offsetWidth * window.devicePixelRatio;
-        canvas.height = canvas.offsetHeight * window.devicePixelRatio;
-        const ctx = canvasRef.current.getContext('2d');
-        const tree = new AlgoTree({
-            ctx,
-            leafType: AlgoTree.BIG_LEAVES,
+        // this is wrapped in a rAF because next does some trickery
+        // with css that cases it to not know the width.
+        requestAnimationFrame(() => {
+            const canvas = canvasRef.current;
+            canvas.width = canvas.offsetWidth * window.devicePixelRatio;
+            canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+            const ctx = canvasRef.current.getContext('2d');
+            const tree = new AlgoTree({
+                ctx,
+                leafType: AlgoTree.BIG_LEAVES,
+            });
+            tree.draw();
         });
-        tree.draw();
     }, []);
     return <canvas ref={canvasRef} className={styles.treeCanvas} />;
 };
